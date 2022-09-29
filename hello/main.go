65c700feee44876730a94383f63e01b1f2e5bf79
z7pz <sdfygamer@gmail.com>
2022-09-29 15:52:57 +0300

    first commit

---
+++ hello/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+}
